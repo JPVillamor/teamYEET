4055abe3c03c56b1c919ee8263adc5d8d5d8b9a7
JP Villamor <jpvillam@mailfence.com>
2022-02-24 13:08:47 -0500

    removed incorrect database saving

--- trydjango/realtime_pr/integers/consumers.py
+++ trydjango/realtime_pr/integers/consumers.py
@@ -1,22 +1,13 @@
 from channels.generic.websocket import WebsocketConsumer
 import json
-from random import randint
 from time import sleep
-from .models import Sensor
-from .models import User
+# from .models import Sensor
+# from .models import User
 from . import counter
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-
-        TempTimestamps = []
-        TempDataValues = []
-        TempTools = []
-
-        NewUser = User(name='JP', bar_mat='wood', tool_selected='drill')
-        NewUser.save()
-        TempSensor = Sensor(name='temperature', unit_name='C', threshold_value=50, user=NewUser)
 
         for i in range(50):
             # self.send(json.dumps({'message': randint(1,100)}))
@@ -27,18 +18,7 @@ class WSConsumer(WebsocketConsumer):
             accyVal = counter.get_acc('y')
             acczVal = counter.get_acc('z')
 
-            TempTimestamps.append(i)
-            TempDataValues.append(tempVal)
-            TempTools.append(NewUser.tool_selected)
-
-            TempSensor.timestamps = TempTimestamps
-            TempSensor.data_values = TempDataValues
-            TempSensor.tool_used = TempTools
-
-            TempSensor.save()
-            
-
-            self.send(json.dumps({'time': i, 'sensor': TempSensor.name, 'value': tempVal, 'unit': TempSensor.unit_name}))
+            self.send(json.dumps({'time': i, 'sensor': 'temp', 'value': tempVal, 'unit': 'C'}))
             self.send(json.dumps({'time': i, 'sensor': 'accx', 'value': accxVal, 'unit': 'm/ss'}))
             self.send(json.dumps({'time': i, 'sensor': 'accy', 'value': accyVal, 'unit': 'm/ss'}))
             self.send(json.dumps({'time': i, 'sensor': 'accz', 'value': acczVal, 'unit': 'm/ss'}))
