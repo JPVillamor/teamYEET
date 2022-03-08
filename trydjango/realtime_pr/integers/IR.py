0e3d529dc561fb44a55e90bcac70d8ff5fd36611
JP <jpvillam@mailfence.com>
2022-03-08 18:20:52 -0500

    refactored IR.py for system integration

--- trydjango/realtime_pr/integers/IR.py
+++ trydjango/realtime_pr/integers/IR.py
@@ -6,13 +6,21 @@ import adafruit_mlx90640
 i2c = busio.I2C(board.SCL, board.SDA, frequency=800000)
 
 mlx = adafruit_mlx90640.MLX90640(i2c)
-print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
+#print("MLX addr detected on I2C", [hex(i) for i in mlx.serial_number])
 
 # if using higher refresh rates yields a 'too many retries' exception,
 # try decreasing this value to work with certain pi/camera combinations
 mlx.refresh_rate = adafruit_mlx90640.RefreshRate.REFRESH_2_HZ
 
 frame = [0] * 768
+
+def get_reading():
+    try:
+        mlx.getFrame(frame)
+    except ValueError:
+        # these happen, no biggie - retry
+
+'''
 while True:
     try:
         mlx.getFrame(frame)
@@ -26,3 +34,4 @@ while True:
             print("%0.1f, " % t, end="")
         print()
     print()
+'''
